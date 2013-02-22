771669ff78797b3829fd23ddc47b30ffc71639e4
Prathyush Katukojwala <prathyush.katukojwala@gmail.com>
2013-02-21 23:16:55 -0800

    No more lag when first laser is shot.

--- interspace-client/ProjectileManager.cpp
+++ interspace-client/ProjectileManager.cpp
@@ -1,12 +1,20 @@
 #include "ProjectileManager.h"
 #include <time.h>
 #include "Laser.h"
+#include "Interspace.h"
 
 template<> ProjectileManager* Ogre::Singleton<ProjectileManager>::msSingleton = 0;
 
 ProjectileManager::ProjectileManager()
 	:bbc(0)
 {
+	Ogre::SceneManager* scenemgr = Interspace::getSingletonPtr()->getSceneManager();
+	scenemgr->getRootSceneNode()->createChild("Projectiles");
+	bbc = scenemgr->createBillboardChain("lasers");
+	scenemgr->getSceneNode("Projectiles")->attachObject(bbc);
+	bbc->setMaxChainElements(2);
+	bbc->setNumberOfChains(1000);
+	bbc->setMaterialName("laser");
 }
 
 ProjectileManager::~ProjectileManager()
@@ -28,22 +36,5 @@ void ProjectileManager::update(double dt)
 
 void ProjectileManager::createLaser(Ogre::SceneManager* scenemgr, Ogre::Vector3 start, Ogre::Vector3 stop)
 {
-	
-	try
-	{
-		scenemgr->getSceneNode("Projectiles");
-	}
-	catch(Ogre::Exception e)
-	{
-		scenemgr->getRootSceneNode()->createChild("Projectiles");
-	}
-	if(!bbc)
-	{
-		bbc = scenemgr->createBillboardChain("projectiles");
-		scenemgr->getSceneNode("Projectiles")->attachObject(bbc);
-		bbc->setMaxChainElements(2);
-		bbc->setNumberOfChains(1000);
-		bbc->setMaterialName("laser");
-	}
 	projectiles.push_back(new Laser(bbc,projectiles.size(),start,stop));
 }
